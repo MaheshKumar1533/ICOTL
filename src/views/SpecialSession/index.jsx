faa94493849995e34fde073bea4de8a9b3404d12
Bodhanapati Bhuvan Kumar <bhuvanb27@gmail.com>
2023-06-16 17:47:31 +0530

    Change about SS

--- src/views/SpecialSession/index.jsx
+++ src/views/SpecialSession/index.jsx
@@ -5,62 +5,82 @@ import EastIcon from "@mui/icons-material/East";
 const SpecialSession = () => {
   return (
     <div className="specialSessionBody">
-      <p className="title">Application for Special Sessions (SS) </p>
+      <p className="title">Call for Special Session Proposals (SS) </p>
       <p className="text">
-        We invite proposals for special sessions (SS) for The First
-        International Conference on Optimization Techniques for Learning (ICOTL
-        2023). The conference will be held in Bengaluru, India, from December 7
-        to December 8, 2023. Special sessions are intended to provide a forum
-        for focused discussions on new or emerging topics of interest in the
-        wide range of applications of optimization techniques in models and
-        training algorithms.
+        We are pleased to announce the invitation for proposals of special
+        sessions (SS) at The First International Conference on Optimization
+        Techniques for Learning (ICOTL 2023). The conference is scheduled to
+        take place in Bengaluru, India, from December 7 to December 8, 2023.
+        Special sessions aim to provide a platform for focused discussions on
+        new or emerging topics of interest within a wide range of applications
+        of optimization techniques in models and training algorithms. The SS
+        organizers have the flexibility to choose between on-site and online
+        presentation formats.
       </p>
-      <p className="subTitle">
-        Proposals should include the following information:
+      <p className="text">
+        We welcome researchers, academicians, and industry experts to submit
+        their proposals for special sessions, highlighting cutting-edge research
+        and developments in optimization techniques for learning. Special
+        sessions offer a unique opportunity to explore novel ideas, exchange
+        knowledge, and foster collaborations in the field.
+      </p>
+      <p className="subTitle">Important Dates:</p>
+      <div>
+        <div className="ssPointsDiv">
+          <EastIcon />
+          <p> Proposal Submission Deadline: July 1, 2023</p>
+        </div>
+        <div className="ssPointsDiv">
+          <EastIcon />
+          <p>Notification of Acceptance: July 8, 2023</p>
+        </div>
+        <div className="ssPointsDiv">
+          <EastIcon />
+          <p> Full Paper Submission Deadline: July 30, 2023</p>
+        </div>
+        <div className="ssPointsDiv">
+          <EastIcon />
+          <p>Conference Dates: December 7-8, 2023</p>
+        </div>
+      </div>
+
+      <p className="subTitle">Submission Guidelines:</p>
+      <p className="text">
+        Prospective organizers of special sessions are requested to submit their
+        proposals electronically in PDF format. The proposal should include the
+        following information:
       </p>
       <div>
         <div className="ssPointsDiv">
           <EastIcon />
-          <p> Title and acronym of the special session</p>
+          <p>Title of the Special Session</p>
         </div>
         <div className="ssPointsDiv">
           <EastIcon />
-          <p>
-            A list of potential topics and subtopics to be covered by the
-            special session
-          </p>
+          <p>Presentation mode (on-site or online)</p>
         </div>
         <div className="ssPointsDiv">
           <EastIcon />
-          <p>Motivation and objectives of the special session</p>
+          <p>Name(s) and affiliation(s) of the organizer(s)</p>
         </div>
         <div className="ssPointsDiv">
           <EastIcon />
-          <p>
-            A short biography of the special session organizer(s), including
-            their affiliation, contact details, and when applicable their
-            previous experiences in organizing similar events
-          </p>
+          <p>A brief biography of the organizer(s)</p>
+        </div>
+        <div className="ssPointsDiv">
+          <EastIcon />
+          <p>Session overview and objectives (approximately 500 words)</p>
         </div>
       </div>
+      <p className="subTitle">Special Session Presentation Options:</p>
       <p className="text">
-        Proposals should be submitted by email to SS chair at
-        chhsieh@gm.cyut.edu.tw within the period from May 1, 2023 to June 1,
-        2023. The subject line should be "ICOTL 2023 Special Session Proposal".
-        ICOTL 2023 Program Committee will evaluate the Proposals based on their
-        originality, significance, quality, and feasibility. The decision
-        notification will be sent by June 8, 2023. Paper submission for SS
-        closes with the paper submission deadline for general session, on 30th.
-        July 2023. Please visit the conference website
-        <a href="http://www.icotl.in/" target="_blank">
-          {" "}
-          www.icotl.in
-        </a>{" "}
-        for details.
+        ICOTL 2023 recognizes the importance of accommodating different
+        presentation preferences. Therefore, special session organizers can
+        choose between on-site or online presentation formats. This flexibility
+        enables participants from around the world to engage in the conference,
+        irrespective of their geographical location.
       </p>
-      <p className="subTitle">
-        Special session organizer(s) will be responsible for:
-      </p>
+      <p>Special session organizer(s) will be responsible for:</p>
       <div>
         <div className="ssPointsDiv">
           <EastIcon />
@@ -73,18 +93,36 @@ const SpecialSession = () => {
         <div className="ssPointsDiv">
           <EastIcon />
           <p>
-            Present a brief introduction and summary of their special session
-            at the conference
+            Present a brief introduction and summary of their special session at
+            the conference
           </p>
         </div>
         <div className="ssPointsDiv">
           <EastIcon />
           <p>
-            Ensuring that their special session adheres to the ethical
-            standards and code of conduct of ICOTL 2023
+            Ensuring that their special session adheres to the ethical standards
+            and code of conduct of ICOTL 2023
           </p>
         </div>
       </div>
+      <p className="text">
+        Proposals should be sent by email to chhsieh@gm.cyut.edu.tw by the
+        specified deadline. All submissions will undergo a rigorous review
+        process by the ICOTL 2023 organizing committee, and acceptance
+        notifications will be sent accordingly.
+      </p>
+      <p className="text">
+        We look forward to receiving your innovative proposals and welcoming you
+        to ICOTL 2023. Together, let us explore the frontiers of optimization
+        techniques for learning and shape the future of this exciting field.
+      </p>
+      <p className="text">
+        For further updates and detailed information, please visit the ICOTL
+        2023 website at <a href="icotl.in">https://icotl.in/.</a>
+      </p>
+      <p className="thankyouNote">
+        Best Regards, <br/> ICOTL 2023 Organizing Committee
+      </p>
     </div>
   );
 };
