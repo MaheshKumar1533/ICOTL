842d21c0b3d65f6fba16cceea22817626330cbd6
HarshaVardhanRao <Hvijapuram@gmail.com>
2025-07-08 15:09:22 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ContactUs from './components/Contact_Us/contactUs';
 import Footer from './components/Footer/footer'
 
 function App() {
-  window.title = "MITS ICOTL' 2026"
   return (
     <div className="App" >
         <Navbar />
